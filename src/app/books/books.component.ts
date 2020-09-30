d697810f0fb9caeb4cf2cf0cb0fdd4ad165035d1
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-09-29 20:55:03 -0700

    created a message service that logs application messages

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Book } from '../book';
-import { BOOKS } from '../mock-books';
 import { BookService } from '../book.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-books',
@@ -11,7 +12,7 @@ import { BookService } from '../book.service';
 export class BooksComponent implements OnInit {
   books: Book[];
   selectedBook: Book;
-  constructor(private bookService: BookService) { }
+  constructor(private bookService: BookService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getBooks();
@@ -19,6 +20,7 @@ export class BooksComponent implements OnInit {
 
   onSelect(book: Book): void {
     this.selectedBook = book;
+    this.messageService.add(`BooksComponent: user has selected book id=${book.id}`);
   }
 
   getBooks(): void {
